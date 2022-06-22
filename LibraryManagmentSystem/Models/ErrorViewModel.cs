d8c8b8327e594ba3fad032961271b845a14ea3bb
VarshaKeshavaPrasad <107409719+VarshaKeshavaPrasad@users.noreply.github.com>
2022-06-22 17:43:58 +0530

    ErrorViewModel

---
+++ LibraryManagmentSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace LibraryManagmentSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public bool IsBookAvailable { get; set; }
+        public bool IsCredentialValid { get; set; }
+    }
+}
