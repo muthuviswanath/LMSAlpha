0a08ae9bd2fe3460d32bef77d708d7dbcb825d5e
VarshaKeshavaPrasad <107409719+VarshaKeshavaPrasad@users.noreply.github.com>
2022-06-26 00:37:13 +0530

    Search Option

--- LibraryManagmentSystem/Controllers/BooksController.cs
+++ LibraryManagmentSystem/Controllers/BooksController.cs
@@ -1,5 +1,13 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using LibraryManagmentSystem.Models;
+using System.Threading.Tasks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Http;
+
 
 namespace LibraryManagmentSystem.Controllers
 {
@@ -8,11 +16,13 @@ namespace LibraryManagmentSystem.Controllers
         private readonly IBooksRepository _booksRepository;
         private readonly IAuthorRepository _authorRepository;
         private readonly IPublisherRepository _publisherRepository;
-        public BooksController(IBooksRepository booksRepository, IAuthorRepository authorRepository,IPublisherRepository publisherRepository)
+        private readonly LibraryManagementContext _context;
+        public BooksController(IBooksRepository booksRepository, IAuthorRepository authorRepository,IPublisherRepository publisherRepository, LibraryManagementContext context)
         {
             _booksRepository = booksRepository;
             _authorRepository = authorRepository;
             _publisherRepository = publisherRepository;
+            _context = context;
         }
         public IActionResult AllBooksList()
         {
@@ -21,6 +31,20 @@ namespace LibraryManagmentSystem.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { IsBookAvailable = false });
+        }
+        public async Task<IActionResult> Search(string SearchString)
+        {
+            var books = from b in _context.Books
+                        select b;
+            
+
+            if (!String.IsNullOrEmpty(SearchString))
+            {
+                books = books.Where(s => s.BookTitle.Contains(SearchString) || s.AuthorInfo.AuthorName.Contains(SearchString) || s.Category.Contains(SearchString));
+                
+            }
+
+            return View(await books.ToListAsync());
         }
     }
 }
